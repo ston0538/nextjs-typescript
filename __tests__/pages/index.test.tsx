799074fa8c3c0e8970b4191607fc1023fbf2025d
KHK <ston0538@gmail.com>
2020-12-17 11:50:57 +0900

    [UPDATE] Modify main component test

--- __tests__/pages/index.test.tsx
+++ __tests__/pages/index.test.tsx
@@ -6,20 +6,10 @@ import userEvent from "@testing-library/user-event";
 afterEach(cleanup);
 
 describe("Index test", () => {
-  const { getByTestId } = render(<Main />);
-  const submitButton = getByTestId("button-test");
-  it("submitButton Click", () => {
-    fireEvent.click(submitButton);
-  });
-  it("File Input Test", () => {
+  it("text content test", () => {
     const { getByTestId, debug } = render(<Main />);
-    const file = new File(["foo"], "foo.png", {
-      type: "image/png",
-    });
-    const inputFile = getByTestId("input-test");
     const result = getByTestId("result");
-    userEvent.upload(inputFile, file);
-    expect(result).toHaveTextContent(/^foo.png$/);
+    expect(result).toHaveTextContent("hi");
     debug();
   });
 });
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,9 @@
 import { useEffect, useRef, useState } from "react";
 import styles from "../styles/Home.module.css";
 export default function Home() {
-  const [image, setImage] = useState<File | null>(null);
-  const [preview, setPreview] = useState<string | null>(null);
-  const fileInputRef = useRef<HTMLInputElement | null>(null);
-  const connectFileInput = (e: React.FormEvent) => {
-    e.preventDefault();
-    fileInputRef.current?.click();
-  };
-  useEffect(() => {
-    if (image) {
-      const reader = new FileReader();
-      reader.readAsDataURL(image);
-      reader.onloadend = () => {
-        setPreview(reader.result as string);
-      };
-    } else {
-    }
-  }, [image]);
   return (
     <div className={styles.container}>
-      <form>
-        {preview ? (
-          <img
-            src={preview}
-            style={{ objectFit: "cover" }}
-            onClick={connectFileInput}
-          />
-        ) : (
-          <button onClick={connectFileInput} data-testid="button-test">
-            Add Image
-          </button>
-        )}
-
-        <input
-          data-testid="input-test"
-          type="file"
-          style={{ display: "none" }}
-          accept="image/*"
-          ref={fileInputRef}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            const file = e.target.files![0];
-            if (file && file.type.substr(0, 5) === "image") {
-              setImage(file);
-            } else {
-              setImage(null);
-            }
-          }}
-        />
-      </form>
+      <p data-testid="result">hi</p>
     </div>
   );
 }
